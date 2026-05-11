b28e863ba0bd1bf5d3d71a90649483d92a78081d
Aditya Agarwal <aaagrawal098@gmail.com>
2026-05-11 18:17:03 +0530

    Udate Supabase configuration

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,8 +3,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_KEY;
+const SUPABASE_URL = "https://uccjotsznspkekflxhku.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjY2pvdHN6bnNwa2VrZmx4aGt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0OTQ5NjksImV4cCI6MjA5NDA3MDk2OX0.98wI7p7mwlWoqZa_kjDjGAZqXEeElKMOkOIs7ntxEm4";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
